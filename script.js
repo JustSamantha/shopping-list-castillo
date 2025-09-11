e05d571f9d5113d4cca501a9a815abc75fce2386
Samantha Castillo <samcastillogf@gmail.com>
2025-09-10 18:44:55 -0700

    - Fixes a couple of bugs - Adds logic to use the local storage - Refactors some code

--- script.js
+++ script.js
@@ -3,21 +3,45 @@ const itemList = document.querySelector('#item-list');
 const clearBtn = document.querySelector('#clear');
 const filterInput = document.querySelector('#filter-input');
 
-function addItem(e) {
-  e.preventDefault();
-  const itemToAdd = document.querySelector('#item-input');
-  const itemList = document.querySelector('#item-list');
-
-  if (itemToAdd === '') {
-    alert('Please enter an item name');
-    return;
+/**
+ * Saves an item to the browser's local storage
+ * @param {String} item Item to be saved to local storage
+ */
+function saveItemToStorage(item) {
+  let shoppingListStorage = JSON.parse(localStorage.getItem('shoppingList'));
+  if (shoppingListStorage === null) {
+    shoppingListStorage = [];
   }
+  fadeIn();
+  shoppingListStorage.push(item);
+  localStorage.setItem('shoppingList', JSON.stringify(shoppingListStorage));
+}
 
+/**
+ * Removes an item from the browser's local storage
+ * @param {String} item Item to be removed from local storage
+ */
+function removeItemFromStorage(item) {
+  const shoppingListStorage = JSON.parse(localStorage.getItem('shoppingList'));
+  const newShoppingList = [];
+  shoppingListStorage.forEach((storedItem) => {
+    if (storedItem !== item) {
+      newShoppingList.push(storedItem);
+    }
+  });
+  localStorage.setItem('shoppingList', JSON.stringify(newShoppingList));
+}
+
+/**
+ * Creates and inserts HTML form an item into the DOM
+ * @param {String} item Item to create the HTML with
+ */
+function addItemHTML(item) {
   const iElement = document.createElement('i');
   iElement.className = 'fa-solid fa-xmark';
   const buttonElement = document.createElement('button');
   buttonElement.className = 'remove-item btn-link text-red';
-  const textNode = document.createTextNode(itemToAdd.value);
+  const textNode = document.createTextNode(item);
   const liElement = document.createElement('li');
 
   buttonElement.appendChild(iElement);
@@ -25,21 +49,55 @@ function addItem(e) {
   liElement.appendChild(buttonElement);
   itemList.appendChild(liElement);
   liElement.addEventListener('click', removeItem);
+}
+
+/**
+ * Function to handle the click event of the add item button
+ * @param {Event} e The event object
+ */
+function addItem(e) {
+  e.preventDefault();
+  const itemToAdd = document.querySelector('#item-input');
+  const itemList = document.querySelector('#item-list');
+
+  if (itemToAdd.value === '') {
+    alert('Please enter an item name');
+    return;
+  }
+
+  addItemHTML(itemToAdd.value);
+  saveItemToStorage(itemToAdd.value);
 
   itemToAdd.value = '';
 }
 
+/**
+ * Function to handle the click event of the remove item button
+ * @param {Event} e The event object
+ */
 function removeItem(e) {
   if (e.target.nodeName === 'I') {
-    e.target.parentElement.parentElement.remove();
+    const mainParent = e.target.parentElement.parentElement;
+    const itemToRemove = mainParent.childNodes[0].textContent;
+    removeItemFromStorage(itemToRemove);
+    mainParent.remove();
   }
 }
 
+/**
+ * Function to handle the click event of the remove all items button
+ * @param {Event} e The event object
+ */
 function removeAllItems(e) {
   const listItems = document.querySelectorAll('#item-list li');
   listItems.forEach((e) => e.remove());
+  localStorage.setItem('shoppingList', JSON.stringify([]));
 }
 
+/**
+ * Function to handle the input event on the filter text box
+ * @param {Event} e The event object
+ */
 function filterList(e) {
   const filterValue = e.target.value.toLowerCase();
   const listItems = document.querySelectorAll('#item-list li');
@@ -52,8 +110,32 @@ function filterList(e) {
       const itemText = e.childNodes[0].textContent.toLowerCase();
       if (!itemText.includes(filterValue)) {
         e.classList.add('hideDisplay');
+      } else {
+        e.classList.remove('hideDisplay');
       }
     });
+  }
+}
+
+/**
+ * Fades in the UI, if there are no items it should be hidden.
+ */
+function fadeIn() {
+  itemList.classList.replace('hideOpacity', 'showOpacity');
+  clearBtn.classList.replace('hideOpacity', 'showOpacity');
+  filterInput.classList.replace('hideOpacity', 'showOpacity');
+}
+
+/**
+ * Checks the local storage and creates the items.
+ */
+function initPage() {
+  const storageList = JSON.parse(localStorage.getItem('shoppingList'));
+  if (storageList !== null && storageList.length > 0) {
+    storageList.forEach((item) => {
+      addItemHTML(item);
+    });
+    fadeIn();
   }
 }
 
@@ -61,9 +143,4 @@ form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', removeAllItems);
 filterInput.addEventListener('input', filterList);
-
-// Fade in the elements if present
-// @todo get elements from localstorage
-itemList.classList.replace('hideOpacity', 'showOpacity');
-clearBtn.classList.replace('hideOpacity', 'showOpacity');
-filterInput.classList.replace('hideOpacity', 'showOpacity');
+initPage();
